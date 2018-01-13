e9db1168e21fc6218876484c40d249a4e1ea8c22
Ian Valle <ianvalle27@gmail.com>
2018-01-13 11:53:25 -0500

    added cozmo commands to prints

--- hangman.py
+++ hangman.py
@@ -1,4 +1,5 @@
 import random
+import cozmo
 from collections import defaultdict
 
 player_names = []
@@ -8,13 +9,16 @@ letters_missed = []
 
 def players():
     print("----------------------")
+	robot.say_text("Welcome to Hangman").wait_for_completed()
     print("Welcome to Hangman!")
     print("----------------------") 
 
     while(True):
         try:
+			robot.say_text("How many players are going to play").wait_for_completed()
             players = int(input("\nHow many players are going to play?")) 
         except ValueError:
+			robot.say_text("That's not a number. Please input a number").wait_for_completed()
             print("That's not a number. Please input a number")
             continue
         else:
@@ -26,11 +30,14 @@ def players():
     more_players = None
 
     while(True):
+		robot.say_text("There are currently " + str(players) + " players. Is that correct? Yes or no?").wait_for_completed()
         more_players = input("\nThere are currently " + str(players) + " players. Is that correct? (Yes/No) ").lower()
         
         if (more_players != "yes" and more_players != "no"):
+			robot.say_text("Wrong input. please input yes or no").wait_for_completed()
             print("Wrong input. Please input yes or no")
         elif (more_players == "no"): 
+			robot.say_text("How many more players will join?").wait_for_completed()
             how_many = int(input("How many more players will join? "))
             add_player_to_list(how_many)
             break
@@ -43,6 +50,7 @@ def players():
     
 def hangman():
     word_to_guess = random_words()
+	robot.say_text("Word to guess: {}" .format(word_to_guess)).wait_for_completed()
     print("Word to guess: {}" .format(word_to_guess))
     word_length = len(word_to_guess)
     guess = ['_' for i in range(word_length)]
@@ -52,8 +60,11 @@ def hangman():
     current_player = 0
 
     print("====================================================")
+	robot.say_text("Let us begin the game").wait_for_completed()
     print("\nLet us begin the game!")
+	robot.say_text("You will have six tries as a group to guess the word").wait_for_completed()
     print("You will have 6 lives as a group to guess the word.")
+	robot.say_text("The current word is {} letters long".format(word_length)).wait_for_completed()
     print("The current word is {} letters long\n" .format(word_length))
     
     while(True):        
@@ -70,9 +81,11 @@ def hangman():
         for i in letters_missed:
             print(i,' ',end='')
 
+		robot.say_text("What is your guess: {}? " .format(player_names[current_player])).wait_for_completed()
         input_guess = input("\nWhat is your guess {}? " .format(player_names[current_player]))
         print("")
         if (len(input_guess) > 1):
+			robot.say_text("Please only input one letter").wait_for_completed()
             print("Please only input 1 letter")
             continue
         elif (input_guess.isalpha()):
@@ -87,6 +100,7 @@ def hangman():
             lives = lives - 1
 
         print("===============")
+		robot.say_text("Current Scores").wait_for_completed()
         print("Current Scores")
         print_player_names(len(player_names))
         print("===============")
@@ -98,6 +112,7 @@ def hangman():
   
 def add_player_to_list(players):
     for name in range(players):
+		robot.say_text("What is your name player: ").wait_for_completed()
         name = (input("What is your name Player: "))
         player_names.append(name)
         player_scores.append(0)
@@ -120,12 +135,16 @@ def check_letter(letter, word_to_guess):
 def check_win_condition(lives, guess, word_to_guess):
 
         if (lives == 0):
+			robot.say_text("You have run out of lives better luck next time").wait_for_completed()
             print("You have run out of lives. Better luck next time!")
+			robot.say_text("The correct word was {}" .format(word_to_guess.upper())).wait_for_completed()
             print("The correct word was {}\n" .format(word_to_guess.upper()))
             
             while(True):
+				robot.say_text("Would you like to play again yes or no").wait_for_completed()
                 play_again = input("Would you like to play again? (Yes/No) ").lower()
                 if (play_again != "yes" and play_again != "no"):
+					robot.say_text("Wrong input. Please input yes or no").wait_for_completed()
                     print("Wrong input. Please input yes or no")
                 elif (play_again == "yes"):
                     print("\n")
@@ -141,14 +160,19 @@ def check_win_condition(lives, guess, word_to_guess):
         
         if (not '_' in guess):
             print("")
+			robot.say_text("Congratulations! You figured out the word").wait_for_completed()
             print("Congratulations! You figured out the word.")
+			robot.say_text("The word was {}").wait_for_completed()
+			#DISPLAY IMAGE
             print("The word was {}" .format(word_to_guess.upper()))
             check_higher_score()
             print("")
 
             while(True):
+				robot.say_text("Would you like to play again? Yes or no").wait_for_completed()
                 play_again = input("Would you like to play again? (Yes/No) ").lower()
                 if (play_again != "yes" and play_again != "no"):
+					robot.say_text("Wrong input. Please input yes or no").wait_for_completed()
                     print("Wrong input. Please input yes or no")
                 elif (play_again == "yes"):
                     print("\n")
@@ -175,9 +199,11 @@ def check_higher_score():
         max_score = max(player_scores)
         max_score_index = player_scores.index(max_score)
 
+		robot.say_text("{} got the highest score with {} points!" .format(player_names[max_score_index], max_score)).wait_for_completed()
         print("{} got the highest score with {} points!" .format(player_names[max_score_index], max_score))
     else:
         max_score = max(player_scores)
+		robot.say_text("Multiple players achieved a high score of {}" .format(max_score)).wait_for_completed()
         print("Multiple players achieved a high score of {}!" .format(max_score))
 
     # dups = defaultdict(list)
@@ -192,8 +218,10 @@ def check_higher_score():
     #         print("got: ", end='')
 
 def print_player_names(players):
+	robot.say_text("The current players are").wait_for_completed()
     print("\nThe current players are: ")
     for item in range(players):
+		robot.say_text(player_names[item]).wait_for_completed()
         print(player_names[item],end=': ') 
         print(player_scores[item])
         

bd49901709a60d6a60691c84b4544a1d9a8dcb35
Rhyan Lugo <rhyanlugocrespo@gmail.com>
2018-01-13 04:35:29 -0500

    implemented score keeping. Need to check for higher score still

--- hangman.py
+++ hangman.py
@@ -1,6 +1,7 @@
 import random
 
 player_names = []
+player_scores = []
 # word_to_guess = None
 letters_missed = []
 
@@ -9,8 +10,16 @@ def players():
     print("Welcome to Hangman!")
     print("----------------------") 
 
-    players = int(input("\nHow many players are going to play?")) 
-
+    while(True):
+        try:
+            players = int(input("\nHow many players are going to play?")) 
+        except ValueError:
+            print("That's not a number. Please input a number")
+            continue
+        else:
+            print()
+            break
+            
     add_player_to_list(players)
 
     more_players = None
@@ -27,45 +36,41 @@ def players():
         elif (more_players == "yes"):
             break
 
-    print_player_names()
+    print_player_names(players)
     hangman()
     # print("The current players are: {}".format(player_names))
     
 def hangman():
     word_to_guess = random_words()
-    print("Word to guess: {}" .format(word_to_guess))
+    # print("Word to guess: {}" .format(word_to_guess))
     word_length = len(word_to_guess)
     guess = ['_' for i in range(word_length)]
     lives = 6
     flag = None
+    number_of_players = len(player_names)
+    current_player = 0
 
     print("====================================================")
     print("\nLet us begin the game!")
     print("You will have 6 lives as a group to guess the word.")
     print("The current word is {} letters long\n" .format(word_length))
     
-    while(True):
+    while(True):        
+        check_win_condition(lives, guess, word_to_guess)
+
         print("Word: ", end='')
         for i in guess:
             print(i,' ', end='')
-            
-        print()
-
-        if (lives == 0):
-            print("\nYou have run out of lives. Better luck next time!")
-            print("The correct word was {}\n" .format(word_to_guess.upper()))
-            exit()
-        if (not '_' in guess):
-            print("\nCongratulations! You figured out the word.")
-            exit()
+        
+        print("")
 
         print("Lives: {}" .format(lives))
         print("Misses: ", end = '')
         for i in letters_missed:
             print(i,' ',end='')
 
-        input_guess = input("\nWhat is your guess? ")
-
+        input_guess = input("\nWhat is your guess {}? " .format(player_names[current_player]))
+        print("")
         if (len(input_guess) > 1):
             print("Please only input 1 letter")
             continue
@@ -76,13 +81,25 @@ def hangman():
             for i in range(word_length):
                 if(word_to_guess[i] == input_guess):
                     guess[i] = input_guess.upper()
+                    player_scores[current_player] += 1
         elif (flag == 0):
             lives = lives - 1
+
+        print("===============")
+        print("Current Scores")
+        print_player_names(len(player_names))
+        print("===============")
+
+        if (current_player >= (number_of_players - 1)):
+            current_player = 0
+        elif (current_player <= (number_of_players - 1)):
+            current_player += 1
   
 def add_player_to_list(players):
     for name in range(players):
         name = (input("What is your name Player: "))
         player_names.append(name)
+        player_scores.append(0)
 
 def random_words():
     words = ["headline", "soup", "filter", "command", "mass", "truck", "tumble", "flourish", "squash", "mouth"]
@@ -98,11 +115,44 @@ def check_letter(letter, word_to_guess):
             letters_missed.append(letter.upper())
     
     return flag
-    
-def print_player_names():
+
+def check_win_condition(lives, guess, word_to_guess):
+
+        if (lives == 0):
+            print("You have run out of lives. Better luck next time!")
+            print("The correct word was {}\n" .format(word_to_guess.upper()))
+            
+            while(True):
+                play_again = input("Would you like to play again? (Yes/No) ").lower()
+                if (play_again != "yes" and play_again != "no"):
+                    print("Wrong input. Please input yes or no")
+                elif (play_again == "yes"):
+                    print("\n")
+                    game()
+                elif (play_again == "no"):
+                    print("")
+                    exit()
+        
+        if (not '_' in guess):
+            print("Congratulations! You figured out the word.")
+
+            while(True):
+                play_again = input("Would you like to play again? (Yes/No) ").lower()
+                if (play_again != "yes" and play_again != "no"):
+                    print("Wrong input. Please input yes or no")
+                elif (play_again == "yes"):
+                    print("\n")
+                    game()
+                elif (play_again == "no"):
+                    print("")
+                    exit()
+
+def print_player_names(players):
     print("\nThe current players are: ")
-    for name in player_names:
-        print(name)
+    for item in range(players):
+        print(player_names[item],end=': ') 
+        print(player_scores[item])
+        
 
 def game():
     players()

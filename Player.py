25cc433a02622cc6a73f495cbb556a9932295e28
Ian Valle <ianvalle27@gmail.com>
2018-01-13 15:25:47 -0500

    module is not callable

--- Player.py
+++ Player.py
@@ -1,6 +1,5 @@
-class Player:
+class Player(object):
 	"""Maintains Player's faces, name and score"""
-	
 	def __init__(self, name = None, points = None):
 		self.name = name
 		self.points = points
